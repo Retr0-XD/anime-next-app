acdd4bd7771c120a99e0bef10d47a1b2196b0d0e
Retr0-XD <88275564+Retr0-XD@users.noreply.github.com>
2023-09-17 12:37:14 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -93,7 +93,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         </div>
       </main>
       <footer className="p-6 text-center text-white/80 sm:p-12">
-      © Annan AIZEN-IN Viluthugal!
+      © Annan AIZEN-IN Viluthugal!!
         
          
        
